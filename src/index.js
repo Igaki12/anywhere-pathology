4bcce8f937b925f940d68435f1a0d482cd99a0f1
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2023-04-10 13:21:04 +0900

    added terms

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from 'react'
 // import ReactDOM from 'react-dom/client';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 const theme = extendTheme({
   styles: {
     global: {
       body: {
-        bg: 'teal.50',
+        bg: 'orange.100',
         // color: 'black',
       },
     },
   },
-});
-const container = document.getElementById('root');
-const root = createRoot(container);
+})
+const container = document.getElementById('root')
+const root = createRoot(container)
 root.render(
   <ChakraProvider theme={theme}>
     <App />
-  </ChakraProvider>
-);
+  </ChakraProvider>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
