58ed319d9774aa6c6c60c549d63755c637136520
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2023-04-19 13:50:57 +0900

    語句を調整

--- src/useQuestionList.js
+++ src/useQuestionList.js
@@ -18,7 +18,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】仮性肥大では実質細胞数は増加するが、その容積は逆に減少している。',
           answerImg: [],
-          answer: '誤...仮性肥大では間質の細胞数が増加し、容積は増加する。',
+          answer: '誤り...仮性肥大では間質の細胞数が増加し、容積は増加する。',
           commentary: '',
         },
         {
@@ -27,7 +27,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】前立腺肥大では実質細胞は過形成だが間質細胞は肥大を示す。',
           answerImg: [prostaticHyperplasia],
-          answer: '誤...間質細胞も過形成される',
+          answer: '誤り...間質細胞も過形成される',
           commentary: '',
         },
         {
@@ -35,7 +35,7 @@ export const useQuestionList = () => {
           questionImg: [],
           questionSentence: '【正誤問題】高血圧では左心室に労働性肥大を生じる',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -45,7 +45,7 @@ export const useQuestionList = () => {
             '【正誤問題】萎縮とは正常の発達段階にあった組織や臓器の細胞数の減少である',
           answerImg: [],
           answer:
-            '誤…萎縮には数的委縮と単純萎縮があり、この文章は数的委縮の説明である。',
+            '誤り...萎縮には数的委縮と単純萎縮があり、この文章は数的委縮の説明である。',
           commentary: '',
         },
         {
@@ -53,7 +53,7 @@ export const useQuestionList = () => {
           questionImg: [],
           questionSentence: '【正誤問題】萎縮には機能障害は関連しない',
           answerImg: [],
-          answer: '誤...機能障害は関連する',
+          answer: '誤り...機能障害は関連する',
           commentary: '',
         },
         {
@@ -62,7 +62,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】低形成とは組織や臓器が正常の大きさまで達しないものである',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -71,7 +71,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】変性とは正常代謝物の異常蓄積や異常代謝物の蓄積が、組織・細胞内に生じることである',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -80,7 +80,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】グルコースはアルドラーゼによりソルビトールを生成し細胞浮腫の原因となる',
           answerImg: [],
-          answer: '誤...アルドラーゼではなくアルドスリダクターゼ',
+          answer: '誤り...アルドラーゼではなくアルドスリダクターゼ',
           commentary: '',
         },
         {
@@ -89,7 +89,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】ポリオール代謝亢進により還元型グルタミンが減少し酸化ストレスが増加する',
           answerImg: [],
-          answer: '誤...還元型グルタミンではなく還元型グルタチオン',
+          answer: '誤り...還元型グルタミンではなく還元型グルタチオン',
           commentary: '',
         },
         {
@@ -98,7 +98,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】ポリオール代謝ではフルクトースから3-デオキシグルコソンが生成されAGE形成が著しい',
           answerImg: [],
-          answer: '誤…ポリオール代謝ではなくフルクトース代謝',
+          answer: '誤り...ポリオール代謝ではなくフルクトース代謝',
           commentary: '',
         },
         {
@@ -106,7 +106,7 @@ export const useQuestionList = () => {
           questionImg: [],
           questionSentence: '【正誤問題】エンドセリンやNOは血管拡張因子である',
           answerImg: [],
-          answer: '誤…エンドセリンは血管収縮因子',
+          answer: '誤り...エンドセリンは血管収縮因子',
           commentary: '',
         },
         {
@@ -115,7 +115,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】糖尿病は続発性拡張型心筋症の原因となる',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -124,7 +124,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】トリグリセリドはグリセロールと2個の脂肪酸からなる',
           answerImg: [],
-          answer: '誤...脂肪酸は3つ',
+          answer: '誤り...脂肪酸は3つ',
           commentary: '',
         },
         {
@@ -133,7 +133,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】血漿リポ蛋白の主成分はカイロミクロン、VLDL、LDL、HDLである',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -142,7 +142,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】アポ蛋白は脂質と結合してリポ蛋白を形成し脂質を可溶化する',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -151,7 +151,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】HDLはコレステロール密度が高く粥状動脈硬化を生じやすい',
           answerImg: [],
-          answer: '誤…HDLではなくLDL',
+          answer: '誤り...HDLではなくLDL',
           commentary: '',
         },
         {
@@ -160,7 +160,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】糖が過剰な状況ではacetyl CoAからトリグリセリドが合成され脂肪細胞に貯蔵される',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -169,7 +169,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】巨大化した脂肪細胞ではアディポネクチンが減少しインスリン耐性が低下する　',
           answerImg: [],
-          answer: '誤…インスリン耐性は増加する',
+          answer: '誤り...インスリン耐性は増加する',
           commentary: '',
         },
         {
@@ -178,7 +178,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】高糖質により増加する酸化ストレスはミトコンドリアの抑制により減少する',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -187,7 +187,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】過剰な遊離脂肪酸は膵β細胞のインスリン分泌を促進し肝での糖新生を亢進する',
           answerImg: [],
-          answer: '誤…インスリン分泌を抑制する',
+          answer: '誤り...インスリン分泌を抑制する',
           commentary: '',
         },
         {
@@ -196,7 +196,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】肝重量の70%以上のトリグリセライドの蓄積により脂肪肝が生じる',
           answerImg: [fattyLiver],
-          answer: '誤…70%ではなく7%',
+          answer: '誤り...70%ではなく7%',
           commentary: '',
         },
         {
@@ -205,7 +205,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】肝細胞障害による脂肪肝では中心肝静脈周囲の肝細胞に脂肪滴を生じやすい',
           answerImg: [fattyLiver],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -214,7 +214,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】脂肪過剰摂取では小葉辺縁性の脂肪肝を生じることがある',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -222,7 +222,7 @@ export const useQuestionList = () => {
           questionImg: [],
           questionSentence: '【正誤問題】脂肪肝では肝間質の脂肪化生が見られる',
           answerImg: [fattyLiver],
-          answer: '誤…肝間質ではなく肝細胞で、脂肪化生ではなく脂肪変性',
+          answer: '誤り...肝間質ではなく肝細胞で、脂肪化生ではなく脂肪変性',
           commentary: '',
         },
         {
@@ -231,7 +231,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】NASHでは小滴脂肪肝、水滴細胞出現、線維化、炎症細胞浸潤が見られる',
           answerImg: [],
-          answer: '誤…小滴脂肪肝ではなく大滴脂肪肝',
+          answer: '誤り...小滴脂肪肝ではなく大滴脂肪肝',
           commentary: '',
         },
         {
@@ -240,7 +240,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】単純性脂肪肝の10%がNASHによりその2%以下が肝硬変に進展すると言われる',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -249,7 +249,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】高トリグリセリド血症ではlarge dense LDLが生成しやすい',
           answerImg: [],
-          answer: '誤...small dense LDLやVLDL',
+          answer: '誤り...small dense LDLやVLDL',
           commentary: '',
         },
         {
@@ -258,7 +258,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】脂肪化生は間葉系幹細胞の脂肪細胞への分化により生ずる場合がある',
           answerImg: [],
-          answer: '誤…脂肪化生という言葉は存在しない',
+          answer: '誤り...脂肪化生という言葉は存在しない',
           commentary: '',
         },
         {
@@ -267,7 +267,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】心筋の脂肪化は致死的不整脈の原因になる',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -275,7 +275,7 @@ export const useQuestionList = () => {
           questionImg: [],
           questionSentence: '【正誤問題】脳内には脂肪細胞は存在しない',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -284,7 +284,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】終末糖化産物は蛋白等にメーラード反応や酸化ストレスなどで非酵素的に糖の修飾が生じたものである',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -293,7 +293,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】終末糖化産物は粥状動脈硬化、脂肪肝、糖尿病合併症、アルツアイマー病、発癌に関与する',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -302,7 +302,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】終末糖化産物は受容体であるRAGEを介し細胞増殖、細胞運動、血管新生を促進する',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -311,7 +311,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】終末糖化産物はDNA付加体の増加をもたらす',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -320,7 +320,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】COX1は外傷や炎症によって誘導され炎症を促進する。Coxib系はこれを阻害する。',
           answerImg: [],
-          answer: '誤…COX1ではなくCOX2',
+          answer: '誤り...COX1ではなくCOX2',
           commentary: '',
         },
         {
@@ -329,7 +329,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】COX1は持続的発現を示し、COX2は発現誘導を受ける。',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -338,7 +338,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】COX2はアラキドン酸をプロスタグランジンへ変換する。',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -347,7 +347,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】COX2は常時様々な組織で発現しており血小板凝集の促進や肝血流の維持に関与している。',
           answerImg: [],
-          answer: '誤...常時ではない',
+          answer: '誤り...常時ではない',
           commentary: '',
         },
         {
@@ -357,7 +357,7 @@ export const useQuestionList = () => {
             '【正誤問題】CpGアイランド（DNA中のCGの繰り返し領域）のメチル化が生じると下流の遺伝子の発現上昇がもたらされる。',
           answerImg: [],
           answer:
-            '誤...CpGアイランドのメチル化によって、下流の遺伝子は発現抑制される',
+            '誤り...CpGアイランドのメチル化によって、下流の遺伝子は発現抑制される',
           commentary:
             '一般的にDNAメチル化は、その下流の遺伝子発現をエピジェネティックに抑制する。',
         },
@@ -367,7 +367,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】CpGアイランドのメチル化によってがん抑制遺伝子の発現が抑制される。',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary:
             '一般的にDNAメチル化は、その下流の遺伝子発現をエピジェネティックに抑制する。',
         },
@@ -377,7 +377,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】Criggler-Najar症候群では間接型ビリルビンが増加する。',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -441,7 +441,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】13トリソミーでは馬蹄腎・臍ヘルニア・揺り椅子状の測定を認めることが多い。',
           answerImg: [],
-          answer: '誤...13ではない',
+          answer: '誤り...13ではない',
           commentary: '',
         },
         {
@@ -450,7 +450,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】1984年以降日本人の死因の第一位はがんである。',
           answerImg: [],
-          answer: '誤...1981年以降',
+          answer: '誤り...1981年以降',
           commentary: '',
         },
         {
@@ -459,7 +459,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】1分子のグルコースから最も多くのATPを生成するのは解糖系である。',
           answerImg: [],
-          answer: '誤...解糖系ではなく電子伝達系',
+          answer: '誤り...解糖系ではなく電子伝達系',
           commentary: '',
         },
         {
@@ -468,7 +468,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】Ⅲ型アレルギーはTcやマクロファージにより好酸球が増殖することで生じる。',
           answerImg: [],
-          answer: '誤...Ⅲ型ではなくⅣ型',
+          answer: '誤り...Ⅲ型ではなくⅣ型',
           commentary: '',
         },
         {
@@ -477,7 +477,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】Ⅳ型アレルギーでは免疫複合体の形成により補体が活性化されることで細胞が傷害される。',
           answerImg: [],
-          answer: '誤...Ⅳ型ではなくⅢ型アレルギー',
+          answer: '誤り...Ⅳ型ではなくⅢ型アレルギー',
           commentary: '',
         },
         {
@@ -486,7 +486,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】AA型アミロイドは血清アミロイドAAタンパクに由来する。',
           answerImg: [],
-          answer: '誤...AA蛋白ではなくA蛋白',
+          answer: '誤り...AA蛋白ではなくA蛋白',
           commentary: '',
         },
         {
@@ -494,7 +494,7 @@ export const useQuestionList = () => {
           questionImg: [],
           questionSentence: '【正誤問題】AGEの増加は大腸がんのリスクとなる',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -503,7 +503,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】Alzheimer病ではβアミロイドが沈着した老人斑により錐体外路障害を認める。',
           answerImg: [],
-          answer: '誤...錐体外路障害ではない',
+          answer: '誤り...錐体外路障害ではない',
           commentary: '',
         },
         {
@@ -741,7 +741,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】COX2はアラキドン酸をプロスタグランジンへ変換する。',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '',
         },
         {
@@ -751,7 +751,7 @@ export const useQuestionList = () => {
             '【正誤問題】α-リノレン酸はn6系の不飽和脂肪酸でアラキドン酸を経てプロスタグランジンになる',
           answerImg: [],
           answer:
-            '誤...α-リノレン酸はn3系不飽和脂肪酸で、DHA・EPAに変換され動脈硬化や血栓の抑止にはたらくといわれている。',
+            '誤り...α-リノレン酸はn3系不飽和脂肪酸で、DHA・EPAに変換され動脈硬化や血栓の抑止にはたらくといわれている。',
           commentary: '',
         },
         {
@@ -760,7 +760,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】ウェルシュ菌は食中毒菌であり腸内には常在しない。',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '加齢とともに腸内に増えるといわれている。',
         },
         {
@@ -769,7 +769,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】ヒドロキシラジカルは脳梗塞や心筋梗塞の病変拡大に寄与する',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: 'ヒドロキシラジカルは活性酸素ROSの中心的な物質。',
         },
         {
@@ -778,7 +778,7 @@ export const useQuestionList = () => {
           questionSentence:
             '【正誤問題】加齢による発がんリスクの上昇は幹細胞分裂回数と関連する',
           answerImg: [],
-          answer: '正',
+          answer: '正しい',
           commentary: '一般的にがん細胞は組織幹細胞から生まれる',
         },
         {
